ed6a3528878235208e007da6351273b4fdfbed16
Beggie98 <bjuraev2020@gmail.com>
2023-06-18 13:11:41 +0500

    Spring: Loose & Tight Coupling

---
+++ Spring-00-Coupling/src/main/java/dono/daryo/loosely_coupled/FullTimeMentor.java
@@ -0,0 +1,7 @@
+package dono.daryo.loosely_coupled;
+
+public class FullTimeMentor implements Mentor {
+    public void createAccount(){
+        System.out.println("Full Time Account is created...");
+    }
+}
