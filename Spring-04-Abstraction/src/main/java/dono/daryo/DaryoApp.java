f6db37e909ea59e946df7ef5545031ae299137cd
Beggie98 <bjuraev2020@gmail.com>
2023-06-22 13:15:17 +0500

    @Qualifier - dependency with different implementations

---
+++ Spring-04-Abstraction/src/main/java/dono/daryo/DaryoApp.java
@@ -0,0 +1,24 @@
+package dono.daryo;
+
+import dono.daryo.config.ProjectConfig;
+import dono.daryo.model.Comment;
+import dono.daryo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DaryoApp {
+
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        CommentService commentService = container.getBean(CommentService.class);
+        commentService.publishComment(comment);
+
+    }
+
+}
