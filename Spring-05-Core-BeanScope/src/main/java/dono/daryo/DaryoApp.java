ab02b9294957135671b978e3e97b08c204b75b7f
Beggie98 <bjuraev2020@gmail.com>
2023-06-22 14:03:17 +0500

    Bean scope and lifecycle: Singleton and prototype

---
+++ Spring-05-Core-BeanScope/src/main/java/dono/daryo/DaryoApp.java
@@ -0,0 +1,32 @@
+package dono.daryo;
+
+import dono.daryo.config.ProjectConfig;
+import dono.daryo.model.Comment;
+import dono.daryo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DaryoApp {
+
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        //CommentService commentService = container.getBean(CommentService.class);
+        //commentService.publishComment(comment);
+
+        CommentService cs1 = container.getBean(CommentService.class);
+        CommentService cs2 = container.getBean(CommentService.class);
+
+        System.out.println(cs1);
+        System.out.println(cs2);
+
+        System.out.println(cs1 == cs2);
+
+    }
+
+}
